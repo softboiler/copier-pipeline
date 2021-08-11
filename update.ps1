273c80c90c4d13530be7fbd3ab4bea155fc1acf8
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-08-10 19:24:56 -0700

    Add a bunch of stuff

---
+++ update.ps1
@@ -0,0 +1,5 @@
+py -3.9 -m venv --clear .venv
+.venv/Scripts/activate
+pip install -U pip  # throws [WinError 5], but still works on its own
+pip install -U setuptools wheel
+pip install -r requirements_dev.txt  # packages for development
