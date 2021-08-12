ecb08991d697fecf61c342272cf226fa8ab89136
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-08-12 11:54:43 -0700

    Update

--- update.ps1
+++
@@ -1,5 +0,0 @@
-py -3.9 -m venv --clear .venv
-.venv/Scripts/activate
-pip install -U pip  # throws [WinError 5], but still works on its own
-pip install -r template/requirements_dev.txt  # packages for development
-flit install -s
