7dd9fe8dba2afadbfe3238ca4433e408b7041286
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-21 18:13:14 -0700

    Fix `bump.py` to copy files, not move them

--- dependabot/bump.py
+++ dependabot/bump.py
@@ -1,6 +1,7 @@
 """Copy bumped workflows to the template folder, appending the `.jinja` suffix."""
 
 from pathlib import Path
+from shutil import copy
 
 COMMON_PATH = ".github/workflows"
 source_folder = Path("./dependabot") / COMMON_PATH
@@ -8,5 +9,4 @@ destination_folder = Path("./template/") / COMMON_PATH
 
 for source in source_folder.iterdir():
     destination = destination_folder / (source.name + ".jinja")
-    destination.unlink()
-    source.rename(destination)
+    copy(source, destination)
