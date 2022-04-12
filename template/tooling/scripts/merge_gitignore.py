cec2ef3a9e2bd365195b7ce46a7bc2ff8b74a81e
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-12 15:57:40 -0700

    Swap "+" for "*" in regex

--- template/tooling/scripts/merge_gitignore.py
+++ template/tooling/scripts/merge_gitignore.py
@@ -5,7 +5,7 @@ gitignore_to_merge = Path("tooling/scripts/gitignore_to_merge")
 gitignore = Path(".gitignore")
 
 lookbehind = r"(?<=^#\[\[\n)"  # look behind for line containing only "# [["
-match = r"[\s\S]+"  # match everything between the above and below tokens
+match = r"[\s\S]*"  # match anything (including nothing) between the tokens
 lookahead = r"(?=^#\]\]\n)"  # look ahead for line containing only "# ]]"
 
 gitignore.write_text(
