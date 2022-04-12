a63956d627f5b5507fdf31ba365d6779efdc8284
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-11 22:01:29 -0700

    This will do for now

--- template/tooling/scripts/merge_gitignore.py
+++ template/tooling/scripts/merge_gitignore.py
@@ -1,1 +1,6 @@
-print("hello!")
+import re
+from pathlib import Path
+
+gitignore_to_merge = Path("tooling/tests/gitignore_to_merge")
+gitignore = Path(".gitignore")
+re.search(r"^#\[\[\n\n(?P<contents>[\s\S]+)\n\n#\]\]", gitignore.read_text(), re.MULTILINE)["contents"]
