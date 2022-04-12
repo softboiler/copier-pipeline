48e779a8c2e96c523a18b50e629b580212701f8b
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-12 14:24:24 -0700

    Finish script

--- template/tooling/scripts/merge_gitignore.py
+++ template/tooling/scripts/merge_gitignore.py
@@ -1,6 +1,18 @@
 import re
 from pathlib import Path
 
-gitignore_to_merge = Path("tooling/tests/gitignore_to_merge")
+gitignore_to_merge = Path("tooling/scripts/gitignore_to_merge")
 gitignore = Path(".gitignore")
-re.search(r"^#\[\[\n\n(?P<contents>[\s\S]+)\n\n#\]\]", gitignore.read_text(), re.MULTILINE)["contents"]
+
+lookbehind = r"(?<=^#\[\[\n)"  # look behind for line containing only "# [["
+match = r"[\s\S]+"  # match everything between the above and below tokens
+lookahead = r"(?=^#\]\]\n)"  # look ahead for line containing only "# ]]"
+
+gitignore.write_text(
+    re.sub(
+        f"{lookbehind}{match}{lookahead}",
+        gitignore_to_merge.read_text(),
+        gitignore.read_text(),
+        flags=re.MULTILINE,
+    )
+)
