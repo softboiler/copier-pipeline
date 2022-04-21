3988d020602fded27d817048eb0303ffb059763c
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-20 20:51:55 -0700

    Change gitignore initialization behavior

--- template/.tools/scripts/merge_gitignore.ps1
+++ template/.tools/scripts/merge_gitignore.ps1
@@ -9,3 +9,6 @@ if(Test-Path -Path .gitignore){
     $original -replace "$lookbehind$match$lookahead", $replace |
         Set-Content -NoNewline -Path .gitignore
 }
+else{
+    Set-Content .gitignore .tools/scripts/.gitignore_init
+}
