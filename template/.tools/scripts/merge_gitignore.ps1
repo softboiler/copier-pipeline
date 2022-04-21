43a2d3411125f28688ed37b1654de710bad5b068
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-20 21:09:21 -0700

    Always create gitignore if it doesn't exist

--- template/.tools/scripts/merge_gitignore.ps1
+++ template/.tools/scripts/merge_gitignore.ps1
@@ -1,14 +1,13 @@
-if(Test-Path -Path .gitignore){
-    $original = Get-Content -Delimiter \0 -Path .gitignore
-    $replace = Get-Content -Delimiter \0 -Path .tools/scripts/.gitignore_to_merge
-
-    $lookbehind = '(?<=\s#\[\[\s)'  # "#[[\n" (have to use \s instead of \n)
-    $match = '[\s\S]*'  # anything in-between
-    $lookahead = '(?=\s#\]\]\s)'  # "#]]\n" (have to use \s instead of \n)
-
-    $original -replace "$lookbehind$match$lookahead", $replace |
-        Set-Content -NoNewline -Path .gitignore
-}
-else{
+if(!(Test-Path -Path .gitignore)){
     Get-Content .tools/scripts/.gitignore_init | Set-Content .gitignore
 }
+
+$original = Get-Content -Delimiter \0 -Path .gitignore
+$replace = Get-Content -Delimiter \0 -Path .tools/scripts/.gitignore_to_merge
+
+$lookbehind = '(?<=\s#\[\[\s)'  # "#[[\n" (have to use \s instead of \n)
+$match = '[\s\S]*'  # anything in-between
+$lookahead = '(?=\s#\]\]\s)'  # "#]]\n" (have to use \s instead of \n)
+
+$original -replace "$lookbehind$match$lookahead", $replace |
+    Set-Content -NoNewline -Path .gitignore
