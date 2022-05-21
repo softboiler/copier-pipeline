b17df3a4e14d631ef1334ca0c3faace79441c937
blakeNaccarato <blake.naccarato@gmail.com>
2022-05-20 21:09:44 -0700

    Try moving template workflows into the root `.github` folder

--- bump.py
+++ bump.py
@@ -3,9 +3,8 @@
 from pathlib import Path
 from shutil import copy
 
-COMMON_PATH = ".github/workflows"
-source_folder = Path("./dependabot") / COMMON_PATH
-destination_folder = Path("./template/") / COMMON_PATH
+source_folder = Path(".github/template")
+destination_folder = Path("template/.github/workflows")
 
 for source in source_folder.iterdir():
     destination = destination_folder / f"{source.name}.jinja"
