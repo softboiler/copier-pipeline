2c5385ae0ad90e6095a0021c51b07f97a3d3749a
blakeNaccarato <blake.naccarato@gmail.com>
2022-05-20 21:25:03 -0700

    Move workflows right into actual workflows directory

--- bump.py
+++ bump.py
@@ -3,9 +3,11 @@
 from pathlib import Path
 from shutil import copy
 
-source_folder = Path(".github/workflows/template")
+source_folder = Path(".github/workflows")
 destination_folder = Path("template/.github/workflows")
 
 for source in source_folder.iterdir():
+    if source.name == "bump_workflows.yml":
+        continue
     destination = destination_folder / f"{source.name}.jinja"
     copy(source, destination)
