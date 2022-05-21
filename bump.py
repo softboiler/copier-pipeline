b73d6d417e7588de56de136a74ced589ea6067a4
blakeNaccarato <blake.naccarato@gmail.com>
2022-05-20 21:35:25 -0700

    Try removing curly braces

--- bump.py
+++ bump.py
@@ -1,13 +1,20 @@
-"""Copy bumped workflows to the template folder, appending the `.jinja` suffix."""
+# """Copy bumped workflows to the template folder, appending the `.jinja` suffix."""
 
-from pathlib import Path
-from shutil import copy
+# from pathlib import Path
+# from shutil import copy
 
-source_folder = Path(".github/workflows")
-destination_folder = Path("template/.github/workflows")
+# source_folder = Path(".github/workflows")
+# destination_folder = Path("template/.github/workflows")
 
-for source in source_folder.iterdir():
-    if source.name == "bump_workflows.yml":
-        continue
-    destination = destination_folder / f"{source.name}.jinja"
-    copy(source, destination)
+# d = {
+#     '          python-version: "python_version"': '          python-version: "{{ python_version }}"',
+#     "          github_token: secrets.GITHUB_TOKEN": "          github_token: ${{ secrets.GITHUB_TOKEN }}",
+#     '          FLIT_PASSWORD: "secrets.PYPI_TOKEN"': '          FLIT_PASSWORD: "{% raw %}${{ secrets.PYPI_TOKEN }}{% endraw %}"',
+# }
+
+
+# for source in source_folder.iterdir():
+#     if source.name == "bump_workflows.yml":
+#         continue
+#     destination = destination_folder / f"{source.name}.jinja"
+#     copy(source, destination)
