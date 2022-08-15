7ebd2531aeeee1dc684dc1cd04de0d5a7a5a1303
blakeNaccarato <blake.naccarato@gmail.com>
2022-08-15 16:24:03 -0700

    Bump pythonrc

--- template/pythonrc.py
+++ template/pythonrc.py
@@ -1,4 +1,28 @@
-from rich import inspect, pretty, print, traceback
+"""Startup for IPython and the REPL. Isn't run for notebooks (see `ipythonrc.py`).
 
-pretty.install()
-traceback.install()
+Avoid activating Rich features that break functionality outside of the REPL.
+"""
+
+
+def main():
+
+    from rich import inspect, traceback
+
+    traceback.install()
+
+    if not is_notebook_or_ipython():
+        from rich import pretty, print
+
+        pretty.install()
+
+
+# https://stackoverflow.com/a/39662359
+def is_notebook_or_ipython() -> bool:
+    try:
+        shell = get_ipython().__class__.__name__  # pyright: ignore
+        return shell == "TerminalInteractiveShell"
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+main()
