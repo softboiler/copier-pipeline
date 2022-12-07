f0c274a5077e51550e5b7bbe5eb62f046652c12b
blakeNaccarato <blake.naccarato@gmail.com>
2022-12-07 09:52:34 -0800

    Ignore more lines

--- template/.ipython/profile_default/startup/ipythonrc.py
+++ template/.ipython/profile_default/startup/ipythonrc.py
@@ -6,7 +6,7 @@ Avoid activating Rich features that break functionality in notebooks.
 
 def main():
     if is_notebook():
-        from rich import inspect, traceback
+        from rich import inspect, traceback  # type: ignore  # For interactive mode
 
         traceback.install()
 
@@ -14,7 +14,7 @@ def main():
 # https://stackoverflow.com/a/39662359
 def is_notebook() -> bool:
     try:
-        shell = get_ipython().__class__.__name__  # pyright: ignore
+        shell = get_ipython().__class__.__name__  # type: ignore  # Dynamic
         return shell == "ZMQInteractiveShell"
     except NameError:
         return False  # Probably standard Python interpreter
--- template/pythonrc.py
+++ template/pythonrc.py
@@ -6,12 +6,12 @@ Avoid activating Rich features that break functionality outside of the REPL.
 
 def main():
 
-    from rich import inspect, traceback
+    from rich import inspect, traceback  # type: ignore  # For interactive mode
 
     traceback.install()
 
     if not is_notebook_or_ipython():
-        from rich import pretty, print
+        from rich import pretty, print  # type: ignore
 
         pretty.install()
 
@@ -19,7 +19,7 @@ def main():
 # https://stackoverflow.com/a/39662359
 def is_notebook_or_ipython() -> bool:
     try:
-        shell = get_ipython().__class__.__name__  # pyright: ignore
+        shell = get_ipython().__class__.__name__  # type: ignore  # Dynamic
         return shell == "TerminalInteractiveShell"
     except NameError:
         return False  # Probably standard Python interpreter
