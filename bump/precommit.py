ff26a10530db52b4a748f0880509afeb56d6feca
blakeNaccarato <blake.naccarato@gmail.com>
2022-12-19 11:03:45 -0800

    Add `pyupgrade` and `blacken-docs`. Bump `black` in `additional_dependencies`, too.

--- bump/precommit.py
+++ bump/precommit.py
@@ -1,18 +1,27 @@
 """Copy bumped pre-commit to the template folder, appending the `.jinja` suffix."""
 
+import re
 from pathlib import Path
 
 source_file = Path("bump/.pre-commit-config.yaml")
 destination_file = Path(
     "template/{% if not override_precommit %}.pre-commit-config.yaml{% endif %}.jinja"
 )
-
+text = source_file.read_text(encoding="utf-8")
+pattern = re.compile(
+    r"""
+  - repo: https:\/\/github\.com\/psf\/black
+    hooks:
+      - id: black
+    rev: "(?P<black_version>[\d.]+)"
+"""
+)
+black_version = pattern.search(text)["black_version"]
 replace = {
     "  #{% if use_dvc -%}": "  {% if use_dvc -%}",
     "  #{% endif -%}": "  {% endif -%}",
+    '        additional_dependencies: ["black=="]': f'        additional_dependencies: ["black=={black_version}"]',
 }
-
-text = source_file.read_text(encoding="utf-8")
 lines = text.split("\n")
 for line_no, line in enumerate(lines):
     if replacement := replace.get(line):
