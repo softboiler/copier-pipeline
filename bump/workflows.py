d45b8e92fb4fcc59716cf4d07b4bc0973c375e20
blakeNaccarato <blake.naccarato@gmail.com>
2022-12-29 17:32:23 -0800

    Add question for which Actions runner to use. Fixes #129

--- bump/workflows.py
+++ bump/workflows.py
@@ -7,6 +7,7 @@ source_folder = Path("bump/workflows")
 destination_folder = Path("template/.github/workflows")
 
 replace = {
+    "    runs-on: ubuntu-latest": "    runs-on: {{ actions_runner }}",
     "          python-version:": '          python-version: "{{ python_version }}"',
     "          github_token:": "          github_token: {% raw %}${{ secrets.GITHUB_TOKEN }}{% endraw %}",
     "          FLIT_PASSWORD:": "          FLIT_PASSWORD: {% raw %}${{ secrets.PYPI_TOKEN }}{% endraw %}",
