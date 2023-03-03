650fe75c65a97aa7c0932f9a0e0deb4e7dcf828e
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-03 10:49:34 -0800

    Correct the order of commands

--- template/update.ps1
+++ template/update.ps1
@@ -11,13 +11,12 @@ else {
 throw [System.Management.Automation.ItemNotFoundException] 'Could not find a virtual environment.'
 }
 
-# Bump `pyproject.toml` with latest dependencies from `requirements.txt`
-python '.tools/scripts/bump_pyproject.py'
-
 # Install dev requirements
 python -m pip install --upgrade pip # Instructed to do this by pip
 pip install --upgrade setuptools wheel # Must be done separately from above
 pip install --upgrade --requirement '.tools/requirements/requirements_dev.txt'
+# Need `toml` in dev requirements prior to bumping `pyproject.toml`
+python '.tools/scripts/bump_pyproject.py'
 
 # Install the package and the lower bound of its requirements
 pip install --no-deps --editable '.'
