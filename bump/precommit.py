e04bed6ecfcf6b4161cc766a3d91477a9d87da08
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-06 12:00:39 -0800

    Introduce nbqa to dev tooling. Still need to add it to pre-commit

--- bump/precommit.py
+++ bump/precommit.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 source_file = Path("bump/.pre-commit-config.yaml")
 destination_file = Path(
-    "template/{% if not override_precommit %}.pre-commit-config.yaml{% endif %}.jinja",
+    "template/{% if not override_precommit %}.pre-commit-config.yaml{% endif %}.jinja"
 )
 text = source_file.read_text(encoding="utf-8")
 pattern = re.compile(
