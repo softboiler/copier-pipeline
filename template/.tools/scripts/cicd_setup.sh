576280db35bf23527546d0531a94ac2b969710bd
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-07 10:50:10 -0800

    Allow user modification of most files

--- template/.tools/scripts/cicd_setup.sh
+++ template/.tools/scripts/cicd_setup.sh
@@ -1,5 +1,5 @@
 python -m pip install --upgrade pip
 pip install --upgrade setuptools wheel
-pip install --requirement .tools/requirements/requirements_cicd.txt
+pip install --requirement .tools/requirements/requirements_ci.txt
 pip install --no-deps .
 pip install --upgrade --requirement 'requirements.txt'
