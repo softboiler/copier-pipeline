e37a34ba8bf355791be6f3123bbe2b0f41adfbc6
blakeNaccarato <blake.naccarato@gmail.com>
2023-03-08 17:04:25 -0800

    Forgot comma

--- template/.tools/scripts/Update-Template.ps1
+++ template/.tools/scripts/Update-Template.ps1
@@ -4,12 +4,12 @@ Update the project to the latest template version.
 
 Param(
     # Whether to update to the latest remote template version
-    [switch]$Remote
+    [switch]$Remote,
 
     # Whether to force templating using old answers.
     [switch]$Force
 )
 
-$(if ($Remote){& '.tools/scripts/template_common.ps1'})
-copier $(if ($Force){'--force'}) --vcs-ref "$(git rev-parse HEAD:template)"
+$(if ($Remote) { & '.tools/scripts/template_common.ps1' })
+copier $(if ($Force) { '--force' }) --vcs-ref "$(git rev-parse HEAD:template)"
 python '.tools/scripts/compose_pyproject.py'
