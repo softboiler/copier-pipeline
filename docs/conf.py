6c27feefaa75b1cb5675143ddeacac7c723ecc6b
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-28 10:06:12 -0700

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,3 +9,14 @@ language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
 extensions = ["myst_parser", "sphinx_design"]
+# https://sphinx-book-theme.readthedocs.io/en/stable/reference.html#reference-of-theme-options
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/blakeNaccarato/copier-python",
+    "repository_branch": "main",
+    "use_issues_button": True,
+    "use_download_button": True,
+    "use_fullscreen_button": True,
+    "use_repository_button": True,
+    "home_page_in_toc": True,
+}
