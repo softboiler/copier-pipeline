a3211aea85eb65d237042af887d90b864db89bb4
blakeNaccarato <blake.naccarato@gmail.com>
2023-05-18 12:15:20 -0700

    Don't install project or look for requirements for top-level template

--- update.ps1
+++ update.ps1
@@ -21,10 +21,6 @@ pip install --upgrade --requirement '.tools/requirements/requirements_dev.txt'
 # Need `toml` in dev requirements prior to bumping `pyproject.toml`
 python '.tools/scripts/compose_pyproject.py'
 
-# Install the package and the lower bound of its requirements
-pip install --no-deps --editable '.'
-pip install --upgrade --requirement 'requirements.txt'
-
 # Ensure pre-commit hooks are applied and updated
 pre-commit install --install-hooks
 
