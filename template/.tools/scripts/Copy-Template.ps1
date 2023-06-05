c4cf5f0f817960fd3b15099a4a32aba67bc5b228
blakeNaccarato <blake.naccarato@gmail.com>
2023-06-05 14:05:43 -0700

    Bump copier requirement

--- template/.tools/scripts/Copy-Template.ps1
+++ template/.tools/scripts/Copy-Template.ps1
@@ -1,7 +1,18 @@
 <#.SYNOPSIS
-Update the project to the latest template version, ignoring project-specific deviations.
+Update the project from the project template.
 #>
 
-& '.tools/scripts/template_common.ps1'
-copier 'gh:blakeNaccarato/copier-python' '.'
+Param(
+    # Whether to update to the latest remote template version
+    [switch]$Update,
+
+    # Whether to recopy the template, ignoring prior diffs, or update in a smart manner.
+    [switch]$Recopy,
+
+    # Whether to use default values for unanswered questions.
+    [switch]$Defaults
+)
+
+$(if ($Update) { & '.tools/scripts/template_common.ps1' })
+copier $(if ($Recopy) { 'recopy' } else { 'update' }) $(if ($Defaults) { '--defaults' }) --vcs-ref "$(git rev-parse HEAD:template)"
 python '.tools/scripts/compose_pyproject.py'
