81720326c8ccedd37a4f5d37778d28c2eebd3655
blakeNaccarato <blake.naccarato@gmail.com>
2023-06-12 10:24:11 -0700

    Don't de-init the template submodule anymore

--- template/.tools/scripts/template_common.ps1
+++ template/.tools/scripts/template_common.ps1
@@ -5,4 +5,3 @@ Common logic for template bumping.
 git submodule update --init --remote --merge template
 git add --all
 git commit -m "Update template digest to $(git rev-parse --short HEAD:template)"
-git submodule deinit template
