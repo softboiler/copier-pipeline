d0b0e621c3b87e64e8dc33247aaf14a14c8540de
blakeNaccarato <blake.naccarato@gmail.com>
2023-07-11 09:56:33 -0700

    Remove unnecessary common script

--- template/.tools/scripts/template_common.ps1
+++
@@ -1,7 +0,0 @@
-<#.SYNOPSIS
-Common logic for template bumping.
-#>
-
-git submodule update --init --remote --merge template
-git add --all
-git commit -m "Update template digest to $(git rev-parse --short HEAD:template)"
