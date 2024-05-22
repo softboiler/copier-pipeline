c4255b684fbab23043250321aab3deb8cefb9368
blakeNaccarato <blake.naccarato@gmail.com>
2024-05-21 17:08:43 -0700

    Lock in more situations

--- template/scripts/{{ project_name }}_tools/types.py
+++ template/scripts/{{ project_name }}_tools/types.py
@@ -6,3 +6,13 @@ Platform: TypeAlias = Literal["linux", "macos", "windows"]
 """Platform."""
 PythonVersion: TypeAlias = Literal["3.9", "3.10", "3.11", "3.12"]
 """Python version."""
+Lock: TypeAlias = dict[
+    str, dict[str, str | bool | tuple[str, ...] | dict[str, dict[str, str]]]
+]
+"""Lockfile."""
+SubmoduleInfoKind = Literal["paths", "urls"]
+"""Submodule info kind."""
+Op = Literal[" @ ", "=="]
+"""Allowable operator."""
+ops: tuple[Op, ...] = (" @ ", "==")
+"""Allowable operators."""
