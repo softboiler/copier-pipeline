9f881451fca3cee655bdb52d634549255cba24d9
blakeNaccarato <blake.naccarato@gmail.com>
2024-07-10 13:11:18 -0700

    Sync with template. Remove unused variable

--- scripts/Sync-Py.ps1
+++ scripts/Sync-Py.ps1
@@ -26,6 +26,7 @@ elseif ($Release) { $msg = 'release' }
 
 $LocalExtensions = '.vscode/extensions'
 $Pylance = 'ms-python.vscode-pylance'
+$PylanceVersion = '2024.6.1'
 if (!$CI -and (Get-Command -Name 'code' -ErrorAction 'Ignore')) {
     'INSTALLING PYLANCE LOCALLY' | Write-Progress
     $Install = @(
